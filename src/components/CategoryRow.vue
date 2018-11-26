832c72609a42b1df6c4ed1cbf4ed902e360b8e6a
Slotigork <maxsoneback@hotmail.com>
2018-11-26 19:08:24 +0100

    Jobbar på single components för category rows

---
+++ src/components/CategoryRow.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div class="category">
+
+    <h2>{{ categoryName }}: </h2>
+
+    <div class="ingredient-wrapper">
+
+      <PlusButton
+      ref="PlusButton"
+      :ingredients="ingredients"
+      :category="category">
+    </PlusButton>
+
+
+  </div>
+</div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'CategoryRow',
+    props: {
+      ingredients: Array,
+      category: Number,
+      categoryName: String
+    },
+    components:{
+      PlusButton
+    },
+    data:function(){
+      return{
+
+      }
+    }
+  }
+
+    import PlusButton from '@/components/PlusButton.vue'
+
+  </script>
+
+  <style scoped>
+  .ingredient-wrapper{
+    text-align: left;
+  }
+  </style>
