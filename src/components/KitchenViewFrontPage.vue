4a88e6a7ca4287bac666cdce165376317250648a
helenalundvall <macbook@student-214-173.eduroam.uu.se>
2018-12-05 12:07:21 +0100

    Knappar KitchenStartView

--- src/components/KitchenViewFrontPage.vue
+++ src/components/KitchenViewFrontPage.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="kitchenFrontPage">
+<div class="masterDivKitchenFront">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+  <div class="kitchenButtons">
       <button id = "grillButton" @click = "emitSwitchVisibility('grillPage')">GRILL</button>
       <button  id = "preppButton" @click = "emitSwitchVisibility('preppPage')">PREPP</button>
   </div>
+
+</div>
 </template>
 
 <script>
@@ -17,21 +22,55 @@ export default {
 </script>
 
 <style scoped>
-.KitchenFrontPage{
-  color: black;
-  padding: 50px;
-  display: grid;
-  font-size: 3vh;
-  border-radius: 10px;
-  margin: 10% auto;
+.masterDivKitchenFront{
+  font-family: 'Montserrat', sans-serif;
+  height:100vh;
+  margin-top:0px !important;
+  padding-top:0px !important;
+  background-color:#f8ffd6;
 }
 
-#favoritesButton{
+#grillButton, #preppButton{
+  width: 25vw;
+  height: 20vh;
+  border-radius: 1vh;
+
+  display: grid;
+  grid-row-gap: 20vh;
+
+  color: black; /*textcolor*/
+  font-size: 5vh;
+
+  cursor: pointer;
+
+  /* width: 50vh;
+  display: grid;
+  grid-row-gap: 8vh;
+  padding: 200px;
+  border: none;
+  margin: none;
+  color: white;
+
+  text-align: center;
+  font-size: 30px;
+
+  cursor: pointer; */
+}
+
+#grillButton{
   background-color: firebrick;
 }
 
-#createBurgerButton {
+#preppButton {
   background-color: royalblue;
+}
+
+button{
+
+}
+
+button:hover{
+
 }
 
 </style>
