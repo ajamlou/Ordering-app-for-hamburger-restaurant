4b19ceabf54c3eec3a10955232d698e516a56042
ajamlou <kevin.ajamlou@gmail.com>
2018-12-05 13:54:34 +0100

    jobbar på prepp

--- src/components/PreppView.vue
+++ src/components/PreppView.vue
@@ -1,26 +1,43 @@
 <template>
   <div class="master-div">
-    <h1>Testheader</h1>
+    <!-- <h1>Testheader</h1> -->
+    <div id="preparing">
+      <div id="header2">
+        <h1>{{ uiLabels.ordersPreparing }}</h1>
+      </div>
+      <div class="allOrders">
+        <OrderItemIsCooking class="isPreparing"
+        v-for="(order, key) in orders"
+        v-if="order.status === 'done'"
+        v-on:cooked="markCooked(key)"
+        :order-id="key"
+        :order="order"
+        :ui-labels="uiLabels"
+        :lang="lang"
+        :key="key">
+      </OrderItemIsCooking>
     </div>
+  </div>
   </div>
 </template>
 
 <script>
-// import sharedVueStuff from '@/components/sharedVueStuff.js'
-// import OrderItemIsCooking from '@/components/OrderItemIsCooking.vue'
+ import sharedVueStuff from '@/components/sharedVueStuff.js'
+ import OrderItemIsCooking from '@/components/OrderItemIsCooking.vue'
 // import OrderItemFinished from '@/components/OrderItemFinished.vue'
 // import OrderItem from '@/components/OrderItem.vue'
 
 export default {
-    // name: 'Ordering',
-    // components: {
+     name: 'Ordering',
+     components: {
     //   OrderItem,
-    //   OrderItemIsCooking,
+       OrderItemIsCooking,
     //   OrderItemFinished,
     //   KitchenViewFrontPage,
-    // },
-  // mixins: [sharedVueStuff]
+     },
+   mixins: [sharedVueStuff]
 }
+
 </script>
 
 <style scoped>
