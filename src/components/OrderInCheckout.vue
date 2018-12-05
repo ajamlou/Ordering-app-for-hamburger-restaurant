2c510748ed1ce15b284ad14696c3eba178e4e921
Slotigork <maxsoneback@hotmail.com>
2018-12-05 13:52:36 +0100

    Lagt till nya saker i ui_en och ui_sv, jobbar vidare med checkout

--- src/components/OrderInCheckout.vue
+++ src/components/OrderInCheckout.vue
@@ -1,5 +1,28 @@
 <template>
-  <h3>{{uiLabels.burger}} {{id}} </h3>
+  <div>
+  <h5>{{uiLabels.burger}} {{id +1}} </h5>
+  <h6>Din burgare:</h6>
+  <div
+  v-for="(ingredient,index) in order.ingredients"
+  v-if="ingredient.category < 5 "
+  :key="index">
+    {{ingredient["ingredient_"+ lang]}}
+  </div>
+  <br>
+
+  <h6>{{uiLabels.extras}}:</h6>
+  <div
+  v-for="(ingredient,index) in order.ingredients"
+  v-if="ingredient.category > 5 "
+  :key="index">
+    {{ingredient["ingredient_"+ lang]}}
+  </div>
+  <div class="ord-check-foot">
+    {{order.price}}
+    <br>
+    {{uiLabels.quantity}}:
+  </div>
+</div>
 </template>
 <script>
 export default{
@@ -9,8 +32,9 @@ export default{
   },
   props:{
     id: Number,
-    item:Object,
-    uiLabels: Array
+    order:Object,
+    uiLabels: Object,
+    lang: String
   },
 }
 </script>
