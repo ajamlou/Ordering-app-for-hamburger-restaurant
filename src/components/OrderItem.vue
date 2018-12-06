5358327c8e9f15c6d6d97579a27f1afc7842a631
Slotigork <maxsoneback@hotmail.com>
2018-12-06 21:13:22 +0100

    Har bug-testat att skicka en order med flera burgare. Det fungerar (woo!) menblir problem när vi ska skicka burgaren till nästa stage

--- src/components/OrderItem.vue
+++ src/components/OrderItem.vue
@@ -1,6 +1,23 @@
 <template>
 <div class = "masterOrderitem">
 
+<!--Förlåt för att jag är här å pillar, men jag var tvungen att se så
+allting fungerade då de nya ordrarna nu inte bara är en array av ingredienser,
+utan en array med arrayer av ingredienser. Det som händer nedan är att order
+tas in som prop (ingen skillnad). Därefter tas this.order.menus = menusArray
+ut i computed, detta blir alltså en array av arrayer. Nedan finns nu 2 divs
+som (1) loopar över menyerna och sedan (2) loopar över ingredienserna
+för sagd array. Jag kommenterar bort allting (men låter det stå kvar)
+så ni inte behöver bry er om följande kod om ni inte vill.
+
+	<div v-for="menu in menusArray">
+		<p>Burgare</p>
+		<div v-for="ingredient in menu.ingredients">
+			{{ingredient.ingredient_sv}}
+		</div>
+	</div>
+-->
+
 	<!-- Bara visa pattys i ordern: -->
 	<div
 	v-if="onlypatty === true">
@@ -14,6 +31,7 @@
 	<div v-else>
 		{{orderId}} {{order.type}} {{ order.ingredients.map(item=>item["ingredient_"+ lang]).join(", ") }}
 	</div>
+
 </div>
 
 
@@ -29,7 +47,13 @@ export default {
     lang: String,
 		onlypatty: Boolean, /*Avgör om det bara är pattys som ska visas*/
 		category: Number		/*För att veta vad som hör till patty-kategorin*/
-  }
+  },
+	computed:{
+		menusArray:function(){
+			let menuArray = this.order.menus;
+			return menuArray;
+		}
+	}
 }
 </script>
 <style scoped>
