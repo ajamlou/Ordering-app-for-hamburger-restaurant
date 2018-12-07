2926101e230f6cf3abf80ae12b81bfcbe986379f
redsuperbat <max.netterberg@gmail.com>
2018-12-07 11:09:55 +0100

    addade fav

---
+++ src/components/Favorite.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div
+    v-for="(ingredient,index) in ingredients"
+    v-if="ingredient.ingredient_id === 3"
+    :key="index">
+      {{ingredient["ingredient_"+ lang]}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Favorite',
+  data: function(){
+    return{
+      bun: "",
+      protein: "",
+      garnish: "",
+      Sauce: ""
+}
+},
+props:{
+ingredients: Array,
+lang: String
+},
+// computed:{
+//   favBurgers.bun: function(){
+//     return this.bun
+//   }
+// },
+methods:{
+}
+}
+</script>
+<style scoped>
+
+.favorites{
+  background-color: white;
+  border:5px solid rgb(255, 0, 0);
+  width: 320px;
+  height: 320px;
+}
+
+.wrapper{
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.header{
+  text-align: center;
+}
+
+.image{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.ingredients{
+  text-align: center;
+  list-style-type: none;
+
+}
+
+
+</style>
