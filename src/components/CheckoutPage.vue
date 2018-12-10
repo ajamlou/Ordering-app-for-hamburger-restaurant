bf83ebf4dda500da4ce680bab422d2e3a2d3670b
Slotigork <maxsoneback@hotmail.com>
2018-12-10 09:59:40 +0100

    försökt göra språkknappen global i ordering

--- src/components/CheckoutPage.vue
+++ src/components/CheckoutPage.vue
@@ -14,11 +14,14 @@
     </slotmodal>
 
     <button @click="goBack">{{uiLabels.back}}</button>
+
     <div id="checkout-title">
       {{uiLabels.yourOrder}}
     </div>
     <div id="btn-order-wrap">
     <div id="add-btn-div" @click="newMenu">
+
+
       <div id="add-btn-top">
         <h5>Klicka för att lägga till en burgare</h5>
       </div>
@@ -95,7 +98,6 @@ export default{
     placeOrder: function () {
       if(this.menus.length>0){
         let menus = {menus: this.menus};
-        // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
         this.$store.state.socket.emit('order', {order: menus});
         this.$emit('clear_all');
       }
@@ -119,12 +121,19 @@ export default{
 </script>
 
 <style scoped>
+#checkout-div{
+  display:grid;
+  grid-template-columns: repeat(8, 1fr);
+}
+
 #btn-order-wrap{
   position:relative;
   display:flex;
   flex-wrap: nowrap;
 }
 #checkout-wrapper{
+  grid-column: 1/7;
+  grid-row: 1;
   display:flex;
   flex-wrap: nowrap;
   text-align: center;
