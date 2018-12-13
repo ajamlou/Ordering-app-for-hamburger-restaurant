c90718e12df6f70fa0734c456bd6d8f1471b23a1
Slotigork <maxsoneback@hotmail.com>
2018-12-13 15:59:03 +0100

    CSS:at frontPage

--- src/components/OrderingViewFrontPage.vue
+++ src/components/OrderingViewFrontPage.vue
@@ -5,10 +5,8 @@
     <div id="welcome">
       {{uiLabels.welcomeText}}
     </div>
-    <div id="buttons">
       <button id = "createBurgerButton" @click = "emitSwitchVisibility('designPage')">{{uiLabels.createYourBurger}}</button>
       <button id = "favoritesButton" @click = "emitSwitchVisibility('favoritesPage')">{{uiLabels.chooseAFavorite}}</button>
-    </div>
   </div>
 </template>
 
@@ -29,31 +27,29 @@ export default {
 <style scoped>
 #frontPage{
   display:grid;
+  grid-template-rows: 6em 30% 30%;
+  grid-row-gap: 1em;
   height:100%;
-}
-#welcome{
-  grid-row:1/2;
-}
-#buttons{
-  grid-row:2/4;
+  justify-items:center;
 }
 #favoritesButton, #createBurgerButton {
   text-transform: uppercase;
   color: white;
-  padding: 5vh;
   display: grid;
   font-size: 4vh;
   border-radius: 3vh;
   border-color: black;
-  margin: 6vh auto auto auto;
   align-items: center;
+  justify-content:center;
   cursor: pointer;
   width: 30vw;
+  min-width:200px;
   height:inherit;
 }
 
 #favoritesButton{
   background-color: #ff884d;
+  grid-row:3/4;
 }
 #favoritesButton:hover {
   background-color: #e64d00;
@@ -64,6 +60,7 @@ export default {
 }
 #createBurgerButton {
   background-color: #00e699;
+  grid-row:2/3;
 }
 #createBurgerButton:hover {
   background-color: #009966;
@@ -80,5 +77,25 @@ export default {
   font-size: 4em;
   color: #66d9ff;
   text-shadow: 2px 2px #0086b3;
+  grid-row:1/2;
+}
+@media screen and (max-width:905px){
+  #frontPage{
+    grid-template-rows: 30% 30% 30%;
+    grid-auto-rows: auto;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+@media screen and (max-width:515px){
+  #welcome{
+    font-size:3em;
+  }
+
+  #favoritesButton, #createBurgerButton{
+    width:4em;
+    font-size: 1.5em;
+  }
 }
 </style>
