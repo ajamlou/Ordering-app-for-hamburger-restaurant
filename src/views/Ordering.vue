64398d36dda24fa926d9bcbeef10838d8530d4b4
Slotigork <maxsoneback@hotmail.com>
2018-12-14 11:43:28 +0100

    Responsiv designPage

--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -75,47 +75,47 @@ v-if="this.showSlotModal">
 class="viewContent"
 id="ordering"
 v-if = "currentView === 'designPage'">
-  <!--<img class="example-panel" src="@/assets/exampleImage.jpg"> -->
+<!--<img class="example-panel" src="@/assets/exampleImage.jpg"> -->
 
-  <div id= "bestallning"><h2>{{ uiLabels.myBurger }}</h2></div>
-  <div id="r2-div"> <!--Div för row 2 i ordering grid -->
-    <div id="gluten-exp">
-      <img src="../assets/gluten.png" class="icon"><span>{{uiLabels.gluten}}</span>
-    </div>
-    <div id="dairy-exp">
-      <img src="../assets/dairy.png" class="icon"><span>{{uiLabels.dairy}}</span>
-    </div>
-    <div id="vegan-exp">
-      <img src="../assets/vegan.png" class="icon"><span>{{uiLabels.vegan}}</span>
-    </div>
+<div id= "bestallning"><h2>{{ uiLabels.myBurger }}</h2></div>
+<div id="r2-div"> <!--Div för row 2 i ordering grid -->
+  <div id="gluten-exp">
+    <img src="../assets/gluten.png" class="icon"><span>{{uiLabels.gluten}}</span>
   </div>
+  <div id="dairy-exp">
+    <img src="../assets/dairy.png" class="icon"><span>{{uiLabels.dairy}}</span>
+  </div>
+  <div id="vegan-exp">
+    <img src="../assets/vegan.png" class="icon"><span>{{uiLabels.vegan}}</span>
+  </div>
+</div>
 
 
-  <div id="categories-wrapper">
-    <CategoryRow v-for="category in burgerCategories"
-    :key="category.categoryNr"
-    :category="category.categoryNr"
-    :added_items="chosenIngredients"
-    :category_name="uiLabels[category.label]"
-    :lang="lang"
-    :threshold="category.threshold"
-    :item_count="categoryItemCounter[category.categoryNr-1]"
-    @remove_ingredient="removeFromMenu"
-    @info_to_modal="toggleShowIngredientsModal">
-  </CategoryRow>
-
-  <h2>{{uiLabels.extras}}</h2>
-
-  <CategoryRow v-for="category in extrasCategories"
+<div id="categories-wrapper">
+  <CategoryRow v-for="category in burgerCategories"
   :key="category.categoryNr"
   :category="category.categoryNr"
   :added_items="chosenIngredients"
   :category_name="uiLabels[category.label]"
   :lang="lang"
   :threshold="category.threshold"
-  :item_count="categoryItemCounter[category.categoryNr -1]"
+  :item_count="categoryItemCounter[category.categoryNr-1]"
   @remove_ingredient="removeFromMenu"
   @info_to_modal="toggleShowIngredientsModal">
+</CategoryRow>
+
+<h2>{{uiLabels.extras}}</h2>
+
+<CategoryRow v-for="category in extrasCategories"
+:key="category.categoryNr"
+:category="category.categoryNr"
+:added_items="chosenIngredients"
+:category_name="uiLabels[category.label]"
+:lang="lang"
+:threshold="category.threshold"
+:item_count="categoryItemCounter[category.categoryNr -1]"
+@remove_ingredient="removeFromMenu"
+@info_to_modal="toggleShowIngredientsModal">
 </CategoryRow>
 </div>
 <div id="price-div">
@@ -331,62 +331,62 @@ export default {
               }
             }
             </script>
-          <style scoped>
-          /* scoped in the style tag means that these rules will only apply to elements, classes and ids in this template and no other templates. */
-          .masterDiv{
-            font-family: 'Montserrat', sans-serif;
-            min-height:100vh;
-            margin-top:0 !important;
-            padding-top:0 !important;
-            background-color:#f8ffd6;
-            display: grid;
-            grid-template-columns: repeat(6, 1fr);
-            grid-template-rows: 100px;
-            grid-auto-rows: auto;
-          }
-          #lang-btn{
-            grid-column:6/7;
-            grid-row:1;
-            color:white;
-            font-weight: 700;
-            width:120px;
-            height:80px;
-            border:1px solid #7a7a7a;
-            margin: auto;
-          }
-          #header-title{
-            grid-column:3/5;
-            grid-row:1;
-            text-align: center;
-            align-self: center;
-          }
-          .viewContent{
-            grid-row: 2;
-            grid-column: 1/7;
-          }
-          #ordering {
-            display:grid;
-            grid-template-columns: repeat(6, 1fr);
-            margin:50px auto auto auto;
-            width: 90%;
-          }
-          #bestallning{
-            grid-column: 1 / 4;
-            grid-row: 1;
-            text-align: left;
-          }
-          #r2-div{
-            grid-row:1/2;
-            grid-column: 4/7;
-            display:grid;
-            grid-template-columns: repeat(3,1fr);
-            grid-template-rows: auto;
-            font-size:1em;
-          }
-          .icon{
-            height:3em;
-            padding:0 3px 3px 0;
-          }
+            <style scoped>
+            /* scoped in the style tag means that these rules will only apply to elements, classes and ids in this template and no other templates. */
+            .masterDiv{
+              font-family: 'Montserrat', sans-serif;
+              min-height:100vh;
+              margin-top:0 !important;
+              padding-top:0 !important;
+              background-color:#f8ffd6;
+              display: grid;
+              grid-template-columns: repeat(6, 1fr);
+              grid-template-rows: 100px;
+              grid-auto-rows: auto;
+            }
+            #lang-btn{
+              grid-column:6/7;
+              grid-row:1;
+              color:white;
+              font-weight: 700;
+              width:120px;
+              height:80px;
+              border:1px solid #7a7a7a;
+              margin: auto;
+            }
+            #header-title{
+              grid-column:3/5;
+              grid-row:1;
+              text-align: center;
+              align-self: center;
+            }
+            .viewContent{
+              grid-row: 2;
+              grid-column: 1/7;
+            }
+            #ordering {
+              display:grid;
+              grid-template-columns: repeat(6, 1fr);
+              margin:50px auto auto auto;
+              width: 90%;
+            }
+            #bestallning{
+              grid-column: 1 / 4;
+              grid-row: 1;
+              text-align: left;
+            }
+            #r2-div{
+              grid-row:1/2;
+              grid-column: 4/7;
+              display:grid;
+              grid-template-columns: repeat(3,1fr);
+              grid-template-rows: auto;
+              font-size:1em;
+            }
+            .icon{
+              height:3em;
+              padding:0 3px 3px 0;
+            }
 
             /*Nedan ser rätt rörigt ut, men det är bara för att det ska funka på alla webbläsare.
             Vi bestämmer en bakgrundsbild och lägger på lite skuggor å sånt*/
@@ -474,32 +474,32 @@ export default {
               grid-row:3;
             }
 
-          #next-btn{
-            width:120px;
-            height:80px;
-            justify-self:end;
-            border:1px solid #7a7a7a;
-            grid-column: 6/7;
-            grid-row:3;
-            color:white;
-            background: -moz-linear-gradient(to bottom, #70db70 51%, #33cc33 51%);
-            background: -webkit-gradient(linear,left top, left bottom, color-stop(51%,#70db70), color-stop(51%,#33cc33));
-            background: -webkit-linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
-            background: -o-linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
-            background: -ms-linear-gradient(top, #70db70 51%,#33cc33 51%);
-            background: linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
-            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#70db70', endColorstr='#33cc33',GradientType=0 );
-          }
-          #next-btn:active{border: 2px solid #595959;}
-          #next-btn:hover{
-            background: -moz-linear-gradient(to bottom, #33cc33 51%, #248f24  51%);
-            /*background: -webkit-gradient(linear,left top, left bottom, color-stop(51%,#ff4d4d), color-stop(51%,#ff0000));*/
-            background: -webkit-linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
-            background: -o-linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
-            background: -ms-linear-gradient(top, #33cc33 51%,#248f24 51%);
-            background: linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
-            filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#33cc33', endColorstr='#248f24',GradientType=0 );
-          }
+            #next-btn{
+              width:120px;
+              height:80px;
+              justify-self:end;
+              border:1px solid #7a7a7a;
+              grid-column: 6/7;
+              grid-row:3;
+              color:white;
+              background: -moz-linear-gradient(to bottom, #70db70 51%, #33cc33 51%);
+              background: -webkit-gradient(linear,left top, left bottom, color-stop(51%,#70db70), color-stop(51%,#33cc33));
+              background: -webkit-linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
+              background: -o-linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
+              background: -ms-linear-gradient(top, #70db70 51%,#33cc33 51%);
+              background: linear-gradient(to bottom, #70db70 51%,#33cc33 51%);
+              filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#70db70', endColorstr='#33cc33',GradientType=0 );
+            }
+            #next-btn:active{border: 2px solid #595959;}
+            #next-btn:hover{
+              background: -moz-linear-gradient(to bottom, #33cc33 51%, #248f24  51%);
+              /*background: -webkit-gradient(linear,left top, left bottom, color-stop(51%,#ff4d4d), color-stop(51%,#ff0000));*/
+              background: -webkit-linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
+              background: -o-linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
+              background: -ms-linear-gradient(top, #33cc33 51%,#248f24 51%);
+              background: linear-gradient(to bottom, #33cc33 51%,#248f24 51%);
+              filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#33cc33', endColorstr='#248f24',GradientType=0 );
+            }
 
 
             .btn-close{
@@ -581,21 +581,37 @@ export default {
               grid-row:4/5;
             }
           }
+          @media screen and (max-width:1206px){
+
+            #ordering{
+              margin:10px auto auto auto;
+            }
+
+          }
           @media screen and (max-width:650px){
             #header-title{
               grid-column:2/6;
             }
             #header-title h1{
-              font-size:2em;
+              font-size:2.3em;
             }
             .icon{
               display:block;
               margin:auto;
+            }
+            #bck-btn,#lang-btn{
+              width:90px;
+              height:50px;
+              padding:0;
+            }
+            h2{
+              font-size:1.8em;
             }
           }
           @media screen and (max-width:480px){
             #next-btn{
               grid-row:4;
             }
+
           }
           </style>
