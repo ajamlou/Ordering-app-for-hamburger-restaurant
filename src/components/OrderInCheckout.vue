95ea855df35b9a794a8b46519c0946413193e150
Slotigork <maxsoneback@hotmail.com>
2019-01-13 14:24:12 +0100

    bättre increment/decrement i orderincheckout

--- src/components/OrderInCheckout.vue
+++ src/components/OrderInCheckout.vue
@@ -2,32 +2,40 @@
   <div class="ord-check-div">
     <h5>{{uiLabels.burger}} {{id +1}} </h5>
     <div class="ord-check-bod">
+      <div
+      v-for="(ingredient,index) in menu.ingredients"
+      v-if="ingredient.category < 5 "
+      :key="index">
+      {{ingredient["ingredient_"+ lang]}}
+    </div>
+    <br>
+
+    <h6 v-if="existsExtras">{{uiLabels.extras}}:</h6>
     <div
     v-for="(ingredient,index) in menu.ingredients"
-    v-if="ingredient.category < 5 "
+    v-if="ingredient.category > 4 "
     :key="index">
     {{ingredient["ingredient_"+ lang]}}
   </div>
-  <br>
-
-  <h6 v-if="existsExtras">{{uiLabels.extras}}:</h6>
-  <div
-  v-for="(ingredient,index) in menu.ingredients"
-  v-if="ingredient.category > 4 "
-  :key="index">
-  {{ingredient["ingredient_"+ lang]}}
-</div>
 </div>
 <div class="ord-check-foot">
-<p>  {{uiLabels.unitprice}}: {{menu.price}}:-</p>
+  <p>  {{uiLabels.unitprice}}: {{menu.price}}:-</p>
   <br>
+
   <div class="ord-quant">
-    <label>{{uiLabels.quantity}}:</label><input
+    <label>{{uiLabels.quantity}}:</label>
+  <div class="num-input">
+
+    <button @click="decrement()" ></button>
+    <input
     type="number"
     v-model="menu.units"
     min="1"
     max="99">
+    <button @click="increment()" class="plus"></button>
   </div>
+</div>
+
   <div class="btns">
     <button @click="removeMenu">{{uiLabels.remove}}</button>
     <button @click="modifyMenu">{{uiLabels.modify}}</button>
@@ -47,6 +55,13 @@ export default{
     lang: String
   },
   methods:{
+    increment:function(){
+      this.menu.units += 1;
+    },
+    decrement:function(){
+      if(this.menu.units != 1){
+      this.menu.units -= 1;}
+    },
     removeMenu:function(){
       this.$emit('remove_menu',this.id);
     },
@@ -99,10 +114,60 @@ export default{
 .ord-quant{
   grid-column:1/3;
   grid-row:2;
+  display:grid;
 }
-.ord-quant input{
-  width:40px;
+.num-input{
+  border: 2px solid #ddd;
+  display: inline-flex;
 }
+
+.num-input,
+.num-input * {
+  box-sizing: border-box;
+}
+
+.num-input input[type=number]{
+  -webkit-appearance: textfield;
+  -moz-appearance: textfield;
+  appearance: textfield;
+  max-width: 5rem;
+  padding: .5rem;
+  border: solid #ddd;
+  border-width: 0 2px;
+  text-align: center;
+}
+.num-input input[type=number]::-webkit-inner-spin-button,
+.num-input input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+.num-input button {
+  outline:none;
+  -webkit-appearance: none;
+  background-color: transparent;
+  border: none;
+  align-items: center;
+  justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  cursor: pointer;
+  margin: 0;
+  position: relative;
+}
+
+.num-input button:before,
+.num-input button:after {
+  display: inline-block;
+  position: absolute;
+  content: '';
+  width: 1rem;
+  height: 2px;
+  background-color: #212121;
+  transform: translate(-50%, -50%);
+}
+.num-input button.plus:after {
+  transform: translate(-50%, -50%) rotate(90deg);
+}
+
 .btns{
   grid-column:1/3;
   grid-row:3;
@@ -110,7 +175,7 @@ export default{
   grid-template-columns: repeat(2,1fr);
 }
 
-button{
+.btns button{
   background-color: #ddd;
   border: none;
   color: black;
