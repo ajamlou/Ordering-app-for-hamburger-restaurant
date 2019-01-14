03b6fa30c748b0c6d1cac27eb3ca783a5542a9d2
ajamlou <kevin.ajamlou@gmail.com>
2019-01-14 14:04:09 +0100

    tillbakaknapp kommer med är du säker-meddelande om breadcrumbs = 1

--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -258,8 +258,10 @@ export default {
                   if(this.isModifying){
                     this.addToCheckout();
                   }
-                  if(this.breadcrumbs.length===1 && this.currentView==='designPage'){
-                    this.clearAll();
+                  if(this.breadcrumbs.length===1){
+                    this.cancelBtnModal();
+                    // this.clearAll();
+                    //this.currentView==='designPage'
                   }
                   if(this.currentView==="favoritesPage"){
                     this.resetBurger();
@@ -268,7 +270,7 @@ export default {
                   if(this.currentView==='checkoutPage'){
                     this.menusArray.pop();
                   }
-                  if(this.breadcrumbs.length>0){
+                  if(this.breadcrumbs.length>1){
                     this.currentView = this.breadcrumbs[this.breadcrumbs.length -1];
                     this.breadcrumbs.pop();
                   }
