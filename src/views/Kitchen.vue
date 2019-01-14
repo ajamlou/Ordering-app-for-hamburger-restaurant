1b25cc9b4e7985313ce894a4c4e98363f596bf32
helenalundvall <macbook@MacbooksMacBook.lan>
2019-01-14 18:17:05 +0100

    Rensat i Kitchen

--- src/views/Kitchen.vue
+++ src/views/Kitchen.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div id="masterDivKitchen">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
@@ -21,7 +20,6 @@
   :uiLabels="uiLabels">
 </KitchenViewFrontPage>
 </div>
-
 
 <!-- Om modalen för ändra favoriter ska visas eller ej -->
 <ChangeFavorites id = "changeFavorites" v-if ="currentView === 'f'"
@@ -55,78 +53,10 @@ v-show="currentView === 'preppPage'"
 </PreppView>
 </div>
 
-<!-- <div id="kitchen-grid" v-show="currentView === 'grillPage'"> -->
-
-<!-- Här skapas beställningarna i "Inkomna". -->
-<!-- <div id="orders">
-<div id="header1">
-<h1>{{ uiLabels.ordersInQueue }}</h1>
-</div>
-<div class="allOrders">
-<OrderItemToPrepare class="toPrepare"
-v-for="(order, key) in orders"
-v-if="order.status === 'not-started'"
-v-on:cancel = "markCanceled(key)"
-v-on:done="markDone(key)"
-:order-id="key"
-:order="order"
-:ui-labels="uiLabels"
-:lang="lang"
-:key="key">
-</OrderItemToPrepare>
-</div>
-</div> -->
-
-<!-- Här skapas beställningarna i "Tillagas". -->
-<!-- <div id="preparing">
-<div id="header2">
-<h1>{{ uiLabels.ordersPreparing }}</h1>
-</div>
-<div class="allOrders">
-<OrderItemIsCooking class="isPreparing"
-v-for="(order, key) in orders"
-v-if="order.status === 'done'"
-v-on:cooked="markCooked(key)"
-:order-id="key"
-:order="order"
-:ui-labels="uiLabels"
-:lang="lang"
-:key="key">
-</OrderItemIsCooking>
-</div>
-</div> -->
-
-<!-- <div class = "backButtonClass" @click="currentView = 'kitchenFrontPage'">
-<button id = "backButton">
-{{ uiLabels.back2 }}</button>
-</div> -->
-
-
-<!-- <div class = "statisticsButtonClass">
-<button  id = "statisticsButton" @click="toggleVisibility(), decideContent('s')">STATISTIK</button>
-</div>
-<div class = "storageButtonClass">
-<button  id = "storageButton" @click="toggleVisibility(), decideContent('l')">LAGER</button>
-</div>
-<div class = "selectButtonClass">
-<button  id = "selectButton">MARKERA</button>
-</div> -->
-
-<!-- Om modalen för statisitk- och lagerknappen ska visas eller ej-->
-<!-- <KitchenModal
-@switchVisibility = "toggleVisibility"
-:decideContent="decidedContent"
-:ingredients = "ingredients"
-v-show = "ModalVisibility === true">
-</KitchenModal> -->
-
 </template>
 <script>
-//import methods and data that are shared between the component and kitchen views
 import KitchenModal from '@/components/KitchenModal.vue'
 import ChangeFavorites from '@/components/ChangeFavorites.vue'
-// import StaffViewStorage from '@/components/StaffViewStorage.vue'
-// import StaffViewStatistics from'@/components/StaffViewStatistics.vue'
 import OrderItem from '@/components/OrderItem.vue'
 import OrderItemToPrepare from '@/components/OrderItemToPrepare.vue'
 import OrderItemIsCooking from '@/components/OrderItemIsCooking.vue'
@@ -139,14 +69,11 @@ import SlotModal from '@/components/SlotModal.vue'
 
 export default {
   name: 'Ordering',
-  // selectAll: false,
   components: {
     OrderItem,
     OrderItemToPrepare,
     OrderItemIsCooking,
     OrderItemFinished,
-    // StaffViewStorage,
-    // StaffViewStatistics,
     KitchenModal,
     KitchenViewFrontPage,
     PreppView,
@@ -160,21 +87,10 @@ export default {
     return {
       chosenIngredients: [],
       price: 0,
-      //ModalVisibility: false,
-      //decidedContent: "statistics",
       currentView: "kitchenFrontPage"
     }
   },
   methods: {
-    // markDone: function (orderid) {
-    //   this.$store.state.socket.emit("orderDone", orderid);
-    // },
-    // markCooked: function (orderid) {
-    //   this.$store.state.socket.emit("orderStarted", orderid);
-    // },
-    // markCanceled: function (orderid) {
-    //   this.$store.state.socket.emit("orderCanceled", orderid);
-    // },
     changeFavorites: function(){
     },
     changeView: function(view){
@@ -190,12 +106,10 @@ export default {
   min-height:100vh;
   margin-top:0 !important;
   padding-top:0 !important;
-  /* background-color: #262626; */
   background: url('../assets/CLP_blackboardSmooth.jpg');
   display: grid;
   grid-template-columns: repeat(6, 1fr);
 }
-
 .contentPage{
   grid-row: 1;
   grid-column: 1/7;
@@ -215,8 +129,10 @@ export default {
   border-radius: 18px;
   background-color: #dc143c;
 }
-#backButton:hover {background-color: #98102b}
-#backButton:active {border: 2px solid grey;}
-
-
+#backButton:hover {
+  background-color: #98102b
+}
+#backButton:active {
+  border: 2px solid grey;
+}
 </style>
